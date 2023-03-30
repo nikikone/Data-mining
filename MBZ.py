01b517825432a8188a6148436446bcf9f783164a
Note PC Nikita <nikitaz0614@gmail.com>
2023-03-31 01:46:04 +1000

    Changes to be committed: 	modified:   MBZ.py 	modified:   test.py

--- MBZ.py
+++ MBZ.py
@@ -1,7 +1,8 @@
 SIZE_KATEGORIAL_CONSTANT = 10
 LEFT_NUM_CONSTANT = 0
 RIGHT_NUM_CONSTANT = 1000
-
+LEFT_CHPD_CONSTANT = 1
+RIGHT_CHPD_CONSTANT = 5
 import numpy as np
 
 class MBZ:
@@ -21,21 +22,52 @@ class MBZ:
         numSize = int(self.numStake * persentStake + remain[0])
         katSize = int(self.katStake * persentStake + remain[1])
         binSize = int(self.binStake * persentStake + remain[2])
-        self.attribute = []
+        self.attributePossibleValues = []
+        self.attributeNormalValues = []
+
         for _ in range(numSize):
-            self.attribute.append(np.random.randint(LEFT_NUM_CONSTANT, RIGHT_NUM_CONSTANT + 1, 2))
+            rand_num = np.random.randint(LEFT_NUM_CONSTANT, RIGHT_NUM_CONSTANT + 1, 2)
+            self.attributePossibleValues.append((np.min(rand_num), np.max(rand_num)))
+            rand_num = np.random.randint(np.min(rand_num), np.max(rand_num) + 1, 2)
+            self.attributeNormalValues.append((np.min(rand_num), np.max(rand_num)))
         
         for _ in range(katSize):
             kategorialSize = np.random.randint(2, SIZE_KATEGORIAL_CONSTANT + 1)
-            kategorial = []    
-            for i in range(1, kategorialSize):
-                kategorial.append('Значение' + str(i))
-            self.attribute.append(kategorial)
+            kategorial, normKategorial = [], []    
+            for i in range(1, kategorialSize + 1):
+                kategorial.append(i)
+            self.attributePossibleValues.append(kategorial.copy())
+            numberNormalValues = np.random.randint(1, kategorialSize - 1)
+            for i in range(numberNormalValues):
+                random_index = np.random.randint(0, len(kategorial))
+                normKategorial.append(kategorial[random_index])
+                kategorial.pop(random_index)
+            self.attributeNormalValues.append(normKategorial)
         
         for _ in range(binSize):
-            self.attribute.append(1)
-        #self.attribute = np.array(self.attribute)
+            self.attributePossibleValues.append(1)
+            self.attributeNormalValues.append(np.random.randint(0, 2))
+
+    def ClassGeneration(self, classSize):
+        self.classSize = classSize
+        self.classValues = []
+        for numClass in range(classSize):
+            mas = []
+            for numAttribute in range(self.attributeSize):
+                rest = [0] * np.random.randint(LEFT_CHPD_CONSTANT, RIGHT_CHPD_CONSTANT + 1)
+                for numPD in range(len(rest)):
+                    if type(self.attributePossibleValues[numAttribute]) is tuple:
+                        thisPD = np.random.randint(*self.attributePossibleValues[numAttribute], 2)
+                        thisPD = (np.min(thisPD), np.max(thisPD))
+                        # Остановился здесь, планировал перегрузить оператор @ для работы с кортежами, но передумал
+                        # Здась требуется создать значения для периодов так, чтобы соседние не пересекались
+                mas.append(rest)
+            self.classValues.append(mas)
         
-a = MBZ(2)
+a = MBZ(3)
 a.AttributeGeneration()
-print(a.attribute)
+a.ClassGeneration(2)
+print(a.classValues)
+print(a.attributePossibleValues)
+#print(a.attributeNormalValues)
+
--- test.py
+++ test.py
@@ -10,4 +10,9 @@ import numpy as np
 #katS = 2
 #stake = numS + binS + katS
 #colNum = numS * (attrSize / 2)
-
+test = (100, 103)
+test_2 = (100, 102)
+print(test @ test_2)
+#print(np.random.randint(*test, 2))
+#for i in range(30):
+#    print(np.random.randint(0, 2))
