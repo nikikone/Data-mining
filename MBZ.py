e88959eccb6794d60b8961519971bf61a2c05727
Note PC Nikita <nikitaz0614@gmail.com>
2023-04-01 00:51:25 +1000

    Changes to be committed: 	modified:   .gitignore 	modified:   MBZ.py 	new file:   requirements.txt 	modified:   test.py

--- MBZ.py
+++ MBZ.py
@@ -1,9 +1,26 @@
+import numpy as np
+import xlsxwriter as xls
+
 SIZE_KATEGORIAL_CONSTANT = 10
 LEFT_NUM_CONSTANT = 0
 RIGHT_NUM_CONSTANT = 1000
 LEFT_CHPD_CONSTANT = 1
 RIGHT_CHPD_CONSTANT = 5
-import numpy as np
+MINIMUM_LENGTH_NUM = 60
+
+# classValues
+[ # <- Классы (Заболевания)
+    [ # <- Признаки
+        ( # <- Значения признака в периодах динамики / (Нижняя граница, Верхняя граница) кол-ва моментов наблюдения
+            [ # <- Массив кортежей, списков или чисел
+    
+            ],
+            [ # < - Кортежи (НГ, ВГ)
+    
+            ]
+        )
+    ]
+]
 
 class MBZ:
     def __init__(self, attr, *, katStake=1, binStake=1, numSatke=1) -> None:
@@ -26,7 +43,9 @@ class MBZ:
         self.attributeNormalValues = []
 
         for _ in range(numSize):
-            rand_num = np.random.randint(LEFT_NUM_CONSTANT, RIGHT_NUM_CONSTANT + 1, 2)
+            rand_num = [0, 0]
+            while abs(rand_num[1] - rand_num[0]) < MINIMUM_LENGTH_NUM:
+                rand_num = np.random.randint(LEFT_NUM_CONSTANT, RIGHT_NUM_CONSTANT + 1, 2)
             self.attributePossibleValues.append((np.min(rand_num), np.max(rand_num)))
             rand_num = np.random.randint(np.min(rand_num), np.max(rand_num) + 1, 2)
             self.attributeNormalValues.append((np.min(rand_num), np.max(rand_num)))
@@ -37,7 +56,10 @@ class MBZ:
             for i in range(1, kategorialSize + 1):
                 kategorial.append(i)
             self.attributePossibleValues.append(kategorial.copy())
-            numberNormalValues = np.random.randint(1, kategorialSize - 1)
+            if kategorialSize - 1 == 1:
+                numberNormalValues = 1    
+            else:
+                numberNormalValues = np.random.randint(1, kategorialSize - 1)
             for i in range(numberNormalValues):
                 random_index = np.random.randint(0, len(kategorial))
                 normKategorial.append(kategorial[random_index])
@@ -56,7 +78,10 @@ class MBZ:
             mas = []
             for numAttribute in range(self.attributeSize):
                 rest = [0] * np.random.randint(LEFT_CHPD_CONSTANT, RIGHT_CHPD_CONSTANT + 1)
+                upAndDownBorder = [0] * len(rest)
                 for numPD in range(len(rest)):
+                    upAndDownBorder[numPD] = np.random.randint(1, 25, 2)
+                    upAndDownBorder[numPD] = (np.min(upAndDownBorder[numPD]), np.max(upAndDownBorder[numPD]))
                     if type(self.attributePossibleValues[numAttribute]) is tuple:
                         checkDifference = False
                         while True:
@@ -88,13 +113,143 @@ class MBZ:
                             rest[0] = np.random.randint(0, 2)
                         else:
                             rest[numPD] = 1 - rest[numPD - 1]
-                mas.append(rest)
+                mas.append((rest, upAndDownBorder))
             self.classValues.append(mas)
+    
+    def ToExcel(self, *, path="data\\", name="MBZ.xlsx"):
+        workbook = xls.Workbook(path + name) # Создать файл
+        worksheet = workbook.add_worksheet('МБЗ') # Создать лист
+
+        column = 0
+        worksheet.write(0, 0, "Заболевания")
+        for row in range(1, self.classSize + 1):
+            worksheet.write(row, column, "Заболевание " + str(row))
+        worksheet.set_column(column, column, 15)
+
+        column += 2
+        worksheet.set_column(column - 1, column - 1, 0.75)
+        worksheet.write(0, column, "Признаки")
+        for row in range(1, self.attributeSize + 1):
+            worksheet.write(row, column, "Признак " + str(row))
+        worksheet.set_column(column, column, 10)
+
+        column += 2
+        worksheet.set_column(column - 1, column - 1, 0.75)
+        worksheet.merge_range(0, column, 0, column + 1, "Возможные значения (ВЗ)")
+        for row in range(1, self.attributeSize + 1):
+            worksheet.write(row, column, "Признак " + str(row))
+            if type(self.attributePossibleValues[row - 1]) is tuple:
+                left, right = self.attributePossibleValues[row - 1]
+                worksheet.write(row, column + 1, '[' + str(int(left)) + '-' + str(int(right)) + ']')
+            elif type(self.attributePossibleValues[row - 1]) is list:
+                listAttrRow = '{'
+                for i in range(len(self.attributePossibleValues[row - 1])):
+                    listAttrRow += "значение " + str(i + 1) + ", "
+                listAttrRow = listAttrRow[:-2] + '}'
+                worksheet.write(row, column + 1, listAttrRow)
+            elif type(self.attributePossibleValues[row - 1]) is int:
+                worksheet.write(row, column + 1, "(0, 1)")
+        worksheet.set_column(column, column, 12)
+        worksheet.set_column(column + 1, column + 1, 12)
+
+        column += 3
+        worksheet.set_column(column - 1, column - 1, 0.75)
+        worksheet.merge_range(0, column, 0, column + 1, "Нормальные значения (НЗ)")
+        for row in range(1, self.attributeSize + 1):
+            worksheet.write(row, column, "Признак " + str(row))
+            if type(self.attributeNormalValues[row - 1]) is tuple:
+                first, second = self.attributeNormalValues[row - 1]
+                worksheet.write(row, column + 1, '[' + str(int(first)) + '-' + str(int(second)) + ']')
+            elif type(self.attributeNormalValues[row - 1]) is list:
+                listAttrRow = '{'
+                for i in range(len(self.attributeNormalValues[row - 1])):
+                    listAttrRow += "значение " + str(i + 1) + ", "
+                listAttrRow = listAttrRow[:-2] + '}'
+                worksheet.write(row, column + 1, listAttrRow)
+            elif type(self.attributeNormalValues[row - 1]) is int:
+                worksheet.write(row, column + 1, self.attributeNormalValues[row - 1])
+        worksheet.set_column(column, column, 12)
+        worksheet.set_column(column + 1, column + 1, 12)
+
+        column += 3
+        worksheet.set_column(column - 1, column - 1, 0.75)
+        worksheet.merge_range(0, column, 0, column + 1, "Клиническая картина (КК)")
+        stepRow = 0
+        for i in range(self.classSize):
+            worksheet.merge_range(1 + stepRow, column, stepRow + self.attributeSize, column, "Заболевание " + str(i + 1))
+            for j in range(self.attributeSize):
+                worksheet.write(j + 1 + stepRow, column + 1, "Признак " + str(j + 1))
+            stepRow += self.attributeSize
+        worksheet.set_column(column, column, 15)
+        worksheet.set_column(column + 1, column + 1, 14)
+
+        column += 3
+        worksheet.set_column(column - 1, column - 1, 0.75)
+        worksheet.merge_range(0, column, 0, column + 2, "Число периодов динамики (ЧПД)")
+        iterRow = 0
+        for classIter in range(self.classSize):
+            for attrIter in range(self.attributeSize):
+                chPD = len(self.classValues[classIter][attrIter][0])
+                worksheet.write(1 + iterRow, column, "Заболевание " + str(classIter + 1))
+                worksheet.write(1 + iterRow, column + 1, "Признак " + str(attrIter + 1))
+                worksheet.write(1 + iterRow, column + 2, chPD)
+                iterRow += 1
+        worksheet.set_column(column, column, 15)
+        worksheet.set_column(column + 1, column + 1, 12)
+        worksheet.set_column(column + 2, column + 2, 12)
+        
+        column += 4
+        worksheet.set_column(column - 1, column - 1, 0.75)
+        worksheet.merge_range(0, column, 0, column + 3, "Значение для периода (ЗДП)")
+        iterRow = 0
+        for classIter in range(self.classSize):
+            for attrIter in range(self.attributeSize):
+                chPD = len(self.classValues[classIter][attrIter][0])
+                for PD in range(chPD):
+                    worksheet.write(1 + iterRow, column, "Заболевание " + str(classIter + 1))
+                    worksheet.write(1 + iterRow, column + 1, "Признак " + str(attrIter + 1))
+                    worksheet.write(1 + iterRow, column + 2, PD + 1)
+                    if type(self.attributeNormalValues[attrIter]) is tuple:
+                        left, right = self.classValues[classIter][attrIter][0][PD]
+                        outPutRow = '[' + str(int(left)) + '-' + str(int(right)) + ']'
+                    elif type(self.attributeNormalValues[attrIter]) is list:
+                        outPutRow = ''
+                        for i in range(len(self.classValues[classIter][attrIter][0][PD])):
+                            outPutRow += 'значение ' + str(self.classValues[classIter][attrIter][0][PD][i]) + ', '
+                        outPutRow = outPutRow[:-2]
+                    elif type(self.attributeNormalValues[attrIter]) is int:
+                        outPutRow = self.classValues[classIter][attrIter][0][PD]
+                    worksheet.write(1 + iterRow, column + 3, outPutRow)
+                    iterRow += 1
+        worksheet.set_column(column, column, 15)
+        worksheet.set_column(column + 1, column + 1, 12)
+        worksheet.set_column(column + 2, column + 2, 12)
+        worksheet.set_column(column + 2, column + 2, 12)
+
+        column += 5
+        worksheet.set_column(column - 1, column - 1, 0.75)
+        worksheet.merge_range(0, column, 0, column + 3, "Верхние и нижние границы (ВГ и НГ)")
+        iterRow = 0
+        for classIter in range(self.classSize):
+            for attrIter in range(self.attributeSize):
+                chPD = len(self.classValues[classIter][attrIter][0])
+                for PD in range(chPD):
+                    worksheet.write(1 + iterRow, column, "Заболевание " + str(classIter + 1))
+                    worksheet.write(1 + iterRow, column + 1, "Признак " + str(attrIter + 1))
+                    worksheet.write(1 + iterRow, column + 2, self.classValues[classIter][attrIter][1][PD][1])
+                    worksheet.write(1 + iterRow, column + 3, self.classValues[classIter][attrIter][1][PD][0])
+                    iterRow += 1
+        worksheet.set_column(column, column, 15)
+        worksheet.set_column(column + 1, column + 1, 12)
+        worksheet.set_column(column + 2, column + 2, 12)
+        worksheet.set_column(column + 2, column + 2, 12)
+
+        workbook.close()
         
 a = MBZ(3)
 a.AttributeGeneration()
-a.ClassGeneration(2)
-print(a.classValues)
-print(a.attributePossibleValues)
+a.ClassGeneration(1)
+#print(a.classValues)
+#print(a.attributePossibleValues)
+#a.ToExcel()
 #print(a.attributeNormalValues)
-
--- test.py
+++ test.py
@@ -1,4 +1,41 @@
 import numpy as np
+import pandas as pd
+import xlsxwriter
+#d = {'col1': [1, 2], 'col2': [3, 4]}
+#d2 = {'col1': [3, 4], 'col2': [5, 6]}
+#df = pd.DataFrame(data=d)
+#df2 = pd.DataFrame(data=d2)
+#writer = pd.ExcelWriter('data\\example.xlsx', engine='xlsxwriter')
+## Записать ваш DataFrame в файл     
+#df.to_excel(writer, 'Sheet1')
+#df2.to_excel(writer, 'Sheet1')
+## Сохраним результат 
+#writer.save()
+workbook = xlsxwriter.Workbook('data\\hello.xlsx') # Создать файл
+worksheet = workbook.add_worksheet() # Создать лист, вы можете указать имя листа с помощью work.add_worksheet ('employee'), но китайское имя будет сообщать об ошибке UnicodeDecodeErro
+expenses = (
+    ['Rent', 1000],
+    ['Gas',   100],
+    ['Food',  300],
+    ['Gym',    50],
+)
+  
+# Начать с первой ячейки. Строки и столбцы индексируются нулем. Запись по метке начинается с 0, а запись по абсолютной позиции "A1" начинается с 1
+row = 0
+col = 0
+  
+# Iterate over the data and write it out row by row.
+for item, cost in (expenses):
+    worksheet.write(row, col,     item)
+    worksheet.write(row, col + 1, cost)
+    row += 1
+  
+# Write a total using a formula.
+worksheet.write(row, 0, 'Total')
+worksheet.write(row, 1, '=SUM(B1:B4)')    # Вызываем формульное выражение excel
+  
+workbook.close()
+
 #attribute = np.zeros((2))
 #print(attribute)
 #attribute[0] = [1, 1]
@@ -10,10 +47,11 @@ import numpy as np
 #katS = 2
 #stake = numS + binS + katS
 #colNum = numS * (attrSize / 2)
-test = [1, 2, 3]
-test_2 = [3, 2, 1, 3]
-c = np.intersect1d(test, test_2)
-print(type(1))
+#test = (1, 2)
+#test_2 = [3, 2, 1, 3]
+#test_3 = (test, test_2)
+#c = np.intersect1d(test, test_2)
+#print(test_3)
 #print(test @ test_2)
 #print(type(test_3) is list)
 #print(np.random.randint(*test, 2))
